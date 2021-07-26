4e01c8279ee03b60cd0e73d3703e7dc873fbf49e
hans <hans.krebs@me.com>
2021-07-26 13:32:26 +0200

    Introduce login-theming with props.

--- components/login/Login.tsx
+++ components/login/Login.tsx
@@ -9,16 +9,20 @@ import {
     Toolbar,
     Link,
 } from "@material-ui/core";
+import {createTheme, ThemeProvider} from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
 import styles from './Login.module.css'
 
 type Props = {
     title: string;
     onLogin: (username: string, password: string) => Promise<boolean>;
     onForgotPasswordClicked?: () => void;
+    primaryColor?: string;
+    errorColor?: string;
 }
 
 function Login(props: Props) {
-    const {title, onLogin, onForgotPasswordClicked} = props;
+    const {title, onLogin, onForgotPasswordClicked, primaryColor, errorColor} = props;
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const [loginError, setLoginError] = useState('');
@@ -34,93 +38,110 @@ function Login(props: Props) {
         }
     }
 
+    const theme = (primaryColor || errorColor) ? createTheme({
+        palette: {
+            ...(primaryColor && {
+                primary: {
+                    main: primaryColor,
+                }
+            }),
+            ...(errorColor && {
+                error: {
+                    main: errorColor,
+                }
+            }),
+        },
+    }) : createTheme();
+
     return (
         <div>
-            <AppBar position="static" color="primary">
-                <Toolbar>
-                    <Grid container justifyContent="center" wrap="wrap">
-                        <Grid item>
-                            <Typography variant="h6">{title}</Typography>
+            <ThemeProvider theme={theme}>
+                <AppBar position="static" color="primary">
+                    <Toolbar>
+                        <Grid container justifyContent="center" wrap="wrap">
+                            <Grid item>
+                                <Typography variant="h6">{title}</Typography>
+                            </Grid>
+                        </Grid>
+                    </Toolbar>
+                </AppBar>
+                <Grid container spacing={0} justifyContent="center" direction="row">
+                    <Grid item>
+                        <Grid
+                            container
+                            direction="column"
+                            justifyContent="center"
+                            spacing={2}
+                            className={styles.loginForm}
+                        >
+                            <Paper
+                                variant="elevation"
+                                elevation={2}
+                                className={styles.loginBackground}
+                            >
+                                <Grid item>
+                                    <Typography component="h1" variant="h5">
+                                        Sign in
+                                    </Typography>
+                                </Grid>
+                                <Grid item>
+                                    <form onSubmit={handleSubmit}>
+                                        <Grid container direction="column" spacing={2}>
+                                            <Grid item>
+                                                <TextField
+                                                    type="email"
+                                                    placeholder="Email"
+                                                    autoComplete="username"
+                                                    fullWidth
+                                                    name="username"
+                                                    variant="outlined"
+                                                    value={username}
+                                                    onChange={(event) => setUsername(event.target.value)}
+                                                    required
+                                                    autoFocus
+                                                />
+                                            </Grid>
+                                            <Grid item>
+                                                <TextField
+                                                    type="password"
+                                                    placeholder="Password"
+                                                    autoComplete="current-password"
+                                                    fullWidth
+                                                    name="password"
+                                                    variant="outlined"
+                                                    value={password}
+                                                    onChange={(event) => setPassword(event.target.value)}
+                                                    required
+                                                />
+                                            </Grid>
+                                            <Grid item>
+                                                <Button
+                                                    variant="contained"
+                                                    color="primary"
+                                                    type="submit"
+                                                    className={styles.buttonBlock}
+                                                >
+                                                    Login
+                                                </Button>
+                                            </Grid>
+                                        </Grid>
+                                    </form>
+                                </Grid>
+                                {onForgotPasswordClicked && <Grid item>
+                                    <Link href="#" variant="body2" onClick={onForgotPasswordClicked} >
+                                        Forgot Password?
+                                    </Link>
+                                </Grid>}
+                                {loginError && <Grid item>
+                                    <Typography component="p" variant="body2" color="error" >
+                                        {loginError}
+                                    </Typography>
+                                </Grid>}
+                            </Paper>
                         </Grid>
                     </Grid>
-                </Toolbar>
-            </AppBar>
-            <Grid container spacing={0} justifyContent="center" direction="row">
-                <Grid item>
-                    <Grid
-                        container
-                        direction="column"
-                        justifyContent="center"
-                        spacing={2}
-                        className={styles.loginForm}
-                    >
-                        <Paper
-                            variant="elevation"
-                            elevation={2}
-                            className={styles.loginBackground}
-                        >
-                            <Grid item>
-                                <Typography component="h1" variant="h5">
-                                    Sign in
-                                </Typography>
-                            </Grid>
-                            <Grid item>
-                                <form onSubmit={handleSubmit}>
-                                    <Grid container direction="column" spacing={2}>
-                                        <Grid item>
-                                            <TextField
-                                                type="email"
-                                                placeholder="Email"
-                                                autoComplete="username"
-                                                fullWidth
-                                                name="username"
-                                                variant="outlined"
-                                                value={username}
-                                                onChange={(event) => setUsername(event.target.value)}
-                                                required
-                                                autoFocus
-                                            />
-                                        </Grid>
-                                        <Grid item>
-                                            <TextField
-                                                type="password"
-                                                placeholder="Password"
-                                                autoComplete="current-password"
-                                                fullWidth
-                                                name="password"
-                                                variant="outlined"
-                                                value={password}
-                                                onChange={(event) => setPassword(event.target.value)}
-                                                required
-                                            />
-                                        </Grid>
-                                        <Grid item>
-                                            <Button
-                                                variant="contained"
-                                                color="primary"
-                                                type="submit"
-                                                className={styles.buttonBlock}
-                                            >
-                                                Login
-                                            </Button>
-                                        </Grid>
-                                    </Grid>
-                                </form>
-                            </Grid>
-                            {onForgotPasswordClicked && <Grid item>
-                                <Link href="#" variant="body2" onClick={onForgotPasswordClicked} >
-                                    Forgot Password?
-                                </Link>
-                            </Grid>}
-                            {loginError && <Grid item>
-                                <Typography component="p" variant="body2" color="error" >
-                                    {loginError}
-                                </Typography>
-                            </Grid>}
-                        </Paper>
-                    </Grid>
                 </Grid>
-            </Grid>
+            </ThemeProvider>
         </div>
     );
 }
